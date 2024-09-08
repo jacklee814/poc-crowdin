cc59af2b477062ce5f47fd1a0fb6444987bd160f
Jack Lee <jacklee82814@gmail.com>
2024-09-08 13:23:42 +0800

    feat: add basic translation

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useTranslation } from "react-i18next";
+
+function App() {
+  const { t, i18n } = useTranslation();
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>{t("title")}</h1>
+      <div>
+        <select
+          onChange={(e) => {
+            i18n.changeLanguage(e.target.value);
+          }}
+        >
+          <option value="en">English</option>
+          <option value="ja">Japanese</option>
+          <option value="th">Thai</option>
+          <option value="zh-hant">Chinese</option>
+        </select>
+      </div>
+    </>
+  );
+}
+
+export default App;
